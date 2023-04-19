e63ca4cc67c4d37eb62ba6f2bc1c9d91b3c3e6d8
mirra-mathias <mathiasmirra@gmail.com>
2023-04-19 15:20:55 +0200

    init

---
+++ src/main/java/fr/clam/edecision/EdecisionApplication.java
@@ -0,0 +1,13 @@
+package fr.clam.edecision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdecisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdecisionApplication.class, args);
+	}
+
+}
